589d5e87f009c42f704badb1594283652b8999dc
root <jhunjhunwala.uday@gmail.com>
2022-02-05 06:02:15 +0000

    basic multisig code

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
-# Basic Sample Hardhat Project
+# Dynamic Multisig Wallet
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat accounts
-npx hardhat compile
-npx hardhat clean
-npx hardhat test
-npx hardhat node
-node scripts/sample-script.js
-npx hardhat help
-```
+This is a simple multisig wallet based on the initial solidity-by-example code. It extends the code by adding ability to include new owner, renounce ownership - in effect turing the contract into a dynamically owned multisig wallet.
---
+++ contracts/Multisig.sol
@@ -0,0 +1,118 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Multisig {
+
+    //Based on solidity-by-example code - extends it by adding the ability to add/renounce ownership, 
+    //and slightly different checks while approving/executing
+    //idea is to make this a dynamic ownership multisig wallet
+    event Deposit(address depositer, uint amount);
+    event Submit(address sender, address to, uint value, bytes data);
+    event Approve(address approver, uint id);
+    event Revoke(address revoker, uint id);
+    event Execute(address executor, uint id);
+    event OwnerAdded(address addedBy, address newOwner);
+    event OwnershipRenounced(address prevOwner);
+
+    struct Transaction {
+        uint id;
+        
+        uint value;
+        uint conf;
+        address to;
+        bytes data;
+        bool executed;
+    }
+
+    uint id_count;
+    Transaction[] transactions;
+    uint public minApprovals;
+    mapping(address=> bool) public owners;
+    uint public numOwners;
+    mapping (uint => mapping (address => bool)) approvalStatus;
+
+
+    constructor() {
+        numOwners++;
+        owners[msg.sender]=true;
+        minApprovals=1;
+    }
+    modifier txExists(uint _id) {
+
+        require(_id < id_count, "No transaction exists with this id");
+        _;
+        
+    }
+
+    modifier onlyOwner (address _addr) {
+
+        require(owners[_addr],"Not owner");
+        _;
+    }
+
+    function addOwner(address _addr) public onlyOwner(msg.sender) {
+        owners[_addr]=true;
+        numOwners++;
+        minApprovals=(numOwners/2)+1;
+        emit OwnerAdded(msg.sender,_addr);
+    }
+    function submitTransaction(address _to, bytes calldata data, uint _value) public onlyOwner(msg.sender){
+        
+        transactions.push(Transaction(id_count,_value,0,_to,data,false));
+        id_count++;
+        emit Submit(msg.sender, _to, _value,data);
+
+    }
+
+    function approveTransaction(uint _id) public txExists(_id) onlyOwner(msg.sender) {
+
+        
+        require(!transactions[_id].executed,"Cannot approve completed transaction");
+        approvalStatus[_id][msg.sender]=true;
+        transactions[_id].conf++;
+        emit Approve(msg.sender,_id);
+    }
+
+    function revokeApproval(uint _id) public txExists(_id) onlyOwner(msg.sender) {
+
+        require(!transactions[_id].executed,"Cannot revoke approval for completed transaction");
+        require(approvalStatus[_id][msg.sender],"No approval to revoke");
+        approvalStatus[_id][msg.sender]=false;
+        transactions[_id].conf--;
+        emit Revoke(msg.sender,_id);
+
+    }
+
+    function executeTransaction(uint _id) public txExists(_id) returns(bool){
+
+        Transaction memory transaction=transactions[_id];
+
+        require(transaction.value < address(this).balance, "Not enough balance for executing transaction");
+        require(transaction.conf >= minApprovals,"Approval quorum not met");
+        (bool status,)=transaction.to.call{value:transaction.value}(transaction.data);
+        emit Execute(msg.sender, _id);
+        return status;
+    }
+
+    function renounceOwnership() public onlyOwner(msg.sender) {
+
+        owners[msg.sender]=false;
+        numOwners--;
+        minApprovals=(numOwners/2)+1;
+        for(uint i=0;i<transactions.length;i++) {
+            if(transactions[i].executed) {
+                continue;
+            }
+            if(approvalStatus[transactions[i].id][msg.sender]) {
+                approvalStatus[transactions[i].id][msg.sender]=false;
+                transactions[i].conf--;
+            }
+        }
+        emit OwnershipRenounced(msg.sender);
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender,msg.value);
+    }
+
+}
